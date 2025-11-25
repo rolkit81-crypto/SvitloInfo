f92e3edd904fc3e46c9c1e06150185963c8b05ee
rolkit81-crypto <rolkit81@gmail.com>
2025-11-25 23:12:15 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ export default {
     "./index.html",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./services/**/*.{js,ts,jsx,tsx}",
-    "./*.{js,ts,jsx,tsx}",
+    "./App.tsx",
+    "./index.tsx"
   ],
   darkMode: 'class',
   theme: {
